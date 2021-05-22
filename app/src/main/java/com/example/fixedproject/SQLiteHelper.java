39391d242491cd8d5e5a501f85fbacbc36f544dc
Yehonatan Assaraf <simaassaarf@gmail.com>
2021-05-22 15:26:03 +0300

    Need to fix Dialog choice at data fix Delete at dals returns of something that isn't bool at dals

--- app/src/main/java/com/example/fixedproject/SQLiteHelper.java
+++ app/src/main/java/com/example/fixedproject/SQLiteHelper.java
@@ -1,6 +1,7 @@
 package com.example.fixedproject;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
@@ -10,11 +11,21 @@ public class SQLiteHelper extends SQLiteAssetHelper {
         super(context, "time_management.db", null, 1);
     }
 
-
     // closing database
     public void closeDB() {
         SQLiteDatabase db = this.getReadableDatabase();
         if (db != null && db.isOpen())
             db.close();
+    }
+    public String SelectType(long _id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("Select type_name From type where _id = ?",null);
+        //return cursor.getString();
+        return null;
+    }
+    public int GetType_id(String type_name){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("Select _id From type where type_name = ?",null);
+        return -1;
     }
 }
